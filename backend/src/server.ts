f42ed622d13fb2916404f25e55eb434fa95b6495
Zetos Lima <cesar_manoel@hotmail.com>
2020-08-25 20:13:44 -0300

    fix: cors initalization.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,28 +7,28 @@ import { enhancedPost } from './util';
 
 const app = express();
 
+app.use(
+  expressPino({
+    logger,
+  })
+);
+app.use(cors());
+app.use(express.json());
+
 app.get('/posts', async (_, res) => {
   const postsRes = await getPosts();
   const usersRes = await getUsers();
-
   const resData = enhancedPost(postsRes.data, usersRes.data);
 
   res.send(JSON.stringify(resData));
 });
 
 app.get('/', async (_, res) => {
-  res.send(JSON.stringify({ hell: true }));
+  res.send(JSON.stringify({ success: true }));
 });
 
 export const Server = {
   start: (port = 3001): void => {
-    app.use(
-      expressPino({
-        logger,
-      })
-    );
-    app.use(express.json());
-    app.use(cors());
     app.listen(port, () => {
       logger.info(`Server listening on port: ${port}`);
     });
