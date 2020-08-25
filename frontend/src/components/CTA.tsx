6277faf9eb708ce61e553f12ee6d7c0d0957d0b3
Zetos Lima <cesar_manoel@hotmail.com>
2020-08-25 17:19:51 -0300

    build: add ts config

--- frontend/src/components/CTA.tsx
+++ frontend/src/components/CTA.tsx
@@ -13,11 +13,11 @@ export const CTA = () => (
   >
     <ChakraLink isExternal href="https://chakra-ui.com" flexGrow={1} mx={2}>
       <Button width="100%" variant="outline" variantColor="green">
-        chakra-ui
+        View on github repo
       </Button>
     </ChakraLink>
 
-    <ChakraLink
+    {/* <ChakraLink
       isExternal
       href="https://github.com/vercel/next.js/blob/canary/examples/with-chakra-ui"
       flexGrow={3}
@@ -26,6 +26,6 @@ export const CTA = () => (
       <Button width="100%" variant="solid" variantColor="green">
         View Repo
       </Button>
-    </ChakraLink>
+    </ChakraLink> */}
   </Container>
 )
