d2b6f6c69eed377cbf2282ec4b878a5408593d74
Zetos Lima <cesar_manoel@hotmail.com>
2020-08-25 19:28:09 -0300

    feat: add axios.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,48 +8,44 @@ import {
   ListItem,
 } from '@chakra-ui/core'
 
+import {useEffect, useState} from 'react';
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Wrapper } from '../components/Wrapper'
+import { Post } from '../components/Post'
+import axios from 'axios';
 
-const Index = () => (
+
+const Index = () => {
+  const posts = [1,2,3];
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   const loadData = async () => {
+  //     const response = await axios.get('http://localhost:3001/', { headers: { Authorization: '*', 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'crossdomain': true} });
+  //     console.log('res:', response.data);
+  //     setPosts(response.data)
+  //   } 
+
+  //   loadData();
+  // })
+
+  return (
   <Container>
-    <Hero />
+    <Hero title={'Growth Tech test'} />
     <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-      </Text>
-
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon icon="check-circle" color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <Icon name="external-link" mx="2px" />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon icon="check-circle" color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <Icon name="external-link" mx="2px" />
-          </ChakraLink>
-        </ListItem>
-      </List>
+      {posts.map((e, index) => (
+        <Wrapper>
+          <Post userName={'John Doe'} companyName={'Netflix'} title={'NiceTitle'} body={'lorem ipsum...'} userId={333} id={32} />
+        </Wrapper>
+      ))}
     </Main>
 
     <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
     <CTA />
   </Container>
-)
+)}
 
 export default Index
