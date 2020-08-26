9087d078905c22bc683dd6fa31cb771e2c1e3d08
Zetos Lima <cesar_manoel@hotmail.com>
2020-08-25 21:11:28 -0300

    feat: fetch on server-side.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,6 @@ import {
   Heading,
 } from '@chakra-ui/core'
 
-import {useEffect, useState} from 'react';
 import { Container } from '../components/Container'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { CTA } from '../components/CTA'
@@ -11,29 +10,36 @@ import { Post, UserPost } from '../components/Post'
 import axios from 'axios';
 
 
-const Index = () => {
-  const [posts, setPosts] = useState<UserPost[]>([]);
-  useEffect(() => {
-    const loadData = async () => {
-      const response = await axios.get<UserPost[]>('http://localhost:3001/posts');
-      console.log('res:', response.data);
-      setPosts(response.data)
-    } 
+const Index = ({ postsList }: {postsList: UserPost[]}) => {
+  // fetch on client
+  // const [posts, setPosts] = useState<UserPost[]>([]);
+  // useEffect(() => {
+  //   const loadData = async () => {
+  //     const response = await axios.get<UserPost[]>('http://localhost:3001/posts');
+  //     setPosts(response.data)
+  //   } 
 
-    loadData();
-  }, []);
+  //   loadData();
+  // }, []);
 
   return (
-  <Container>
+    <Container>
       <Heading>Growth Tech test</Heading>
-      {posts.map((post, _) => (
+      {postsList.map((post, _) => (
         <Wrapper key={post.id}>
           <Post userName={post.userName} companyName={post.companyName} title={post.title} body={post.body} userId={post.userId} id={post.id} />
         </Wrapper>
       ))}
-    <DarkModeSwitch />
-    <CTA />
-  </Container>
-)}
+      <DarkModeSwitch />
+      <CTA />
+    </Container>
+  )
+}
+
+Index.getInitialProps = async () => {
+  // fetch on server
+  const response = await axios.get<UserPost[]>('http://localhost:3001/posts');
+  return { postsList: response.data }
+}
 
 export default Index
