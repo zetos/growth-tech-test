259697b405cc0ca3b467fc703829a8de10beb2c8
Zetos Lima <cesar_manoel@hotmail.com>
2020-08-25 22:42:52 -0300

    fix: add createServer function.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,38 @@
-import express, { Express } from 'express';
+import express, { Application } from 'express';
 import expressPino from 'express-pino-logger';
 import cors from 'cors';
 import logger from './logger';
 import { getUsers, getPosts } from './clients/jsonPlaceHolder';
 import { enhancedPost } from './util';
 
-const app = express();
+export const createServer = (): Application => {
+  const app = express();
+  app.use(
+    expressPino({
+      logger,
+    })
+  );
+  app.use(cors());
+  app.use(express.json());
 
-app.use(
-  expressPino({
-    logger,
-  })
-);
-app.use(cors());
-app.use(express.json());
+  app.get('/posts', async (_, res) => {
+    const postsRes = await getPosts();
+    const usersRes = await getUsers();
+    const resData = enhancedPost(postsRes.data, usersRes.data);
 
-app.get('/posts', async (_, res) => {
-  const postsRes = await getPosts();
-  const usersRes = await getUsers();
-  const resData = enhancedPost(postsRes.data, usersRes.data);
+    res.send(JSON.stringify(resData));
+  });
 
-  res.send(JSON.stringify(resData));
-});
-
-app.get('/', async (_, res) => {
-  res.send(JSON.stringify({ success: true }));
-});
+  app.get('/', async (_, res) => {
+    res.send({ success: true });
+  });
+  return app;
+};
 
 export const Server = {
   start: (port = 3001): void => {
-    app.listen(port, () => {
+    createServer().listen(port, () => {
       logger.info(`Server listening on port: ${port}`);
     });
   },
-  getApp: (): Express => app,
 };
